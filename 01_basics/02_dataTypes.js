1e177cbec3f915b308d502df67aebb9acead6a7f
edishcake <manishssr6@gmail.com>
2024-09-23 07:33:10 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS codes as newer version
+
+//alert(3+3) // we are using nodeJS not a browser
+
+console.log(3
+    +
+    3) // code redability should be high
+
+console.log("Rakesh");
+
+let name = "rakesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof undefined);
