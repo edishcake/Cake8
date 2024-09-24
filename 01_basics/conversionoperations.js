07019ec4c0da16142ddd701fea27243906973940
edishcake <manishssr6@gmail.com>
2024-09-24 10:34:22 +0000

    conversions

---
+++ 01_basics/conversionoperations.js
@@ -0,0 +1,43 @@
+
+let score = ""
+
+console.log(typeof score);
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN not a number
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1=> true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringsomeNumber = String(someNumber)
+
+console.log(stringsomeNumber);
+console.log(typeof stringsomeNumber);
+
+//************************************* */
+
+//console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
+
