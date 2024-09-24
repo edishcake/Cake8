7db4fa0066eeff376c82f394295e6cb7e5bba2a1
edishcake <manishssr6@gmail.com>
2024-09-24 12:20:29 +0000

    data

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,42 @@
+// const isloggedIn = null
+// let tempOutside;
+// let scoreValue = undefined
+
+// console.log(isloggedIn);
+// console.log(tempOutside);
+// console.log(scoreValue);
+
+
+//primitive datatypes (call by value)
+
+//strings, numbers, boolean, null, undefined, symbol, bigint 
+
+//non-primitive datatypes (call by reference)
+
+// objects, arrays, functions
+
+const classStrength = 100
+const roomTemperature = 37.5
+const userName = "Rakesh"
+const emailAddres = undefined
+const bigNumber = 134341515135n
+const dayOff = true
+
+const Id = Symbol('124')
+const anotherId = Symbol('124')
+
+console.log(Id === anotherId);
+ 
+// console.log(emailAddres);
+
+// const employee = ["name", "age", "address"];
+
+// let myObj = {
+//     name: rakesh,
+//     age: 21,
+//     class: D
+// }
+
+let myFunction = function(){
+    console.log("hello word");
+    }
